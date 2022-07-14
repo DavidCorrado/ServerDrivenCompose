8517247cc12e3e98788be31b619ccf702fab3071
david.corrado <david.corrado@well.co>
2022-07-14 17:48:42 -0500

    Fix differences between platforms

--- app/src/main/java/com/davidcorrado/serverdriven/ui/sd/view/SDCard.kt
+++ app/src/main/java/com/davidcorrado/serverdriven/ui/sd/view/SDCard.kt
@@ -1,5 +1,6 @@
 package com.davidcorrado.serverdriven.ui.sd.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -7,5 +8,9 @@ import com.davidcorrado.serverdriven.ui.sd.data.ServerCard
 
 @Composable
 fun SDCard(serverCard: ServerCard, scope: Any?, content: @Composable () -> Unit) {
-    Card(modifier = serverCard.modifier?.toModifier(scope) ?: Modifier, content = content)
+    Card(modifier = serverCard.modifier?.toModifier(scope) ?: Modifier) {
+        Column {
+            content.invoke()
+        }
+    }
 }
--- app/src/main/java/com/davidcorrado/serverdriven/views/Testing.kt
+++ app/src/main/java/com/davidcorrado/serverdriven/views/Testing.kt
@@ -1,14 +1,33 @@
 package com.davidcorrado.serverdriven.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.davidcorrado.serverdriven.ui.sd.data.ServerCard
-import com.davidcorrado.serverdriven.ui.sd.data.ServerModifier
-import com.davidcorrado.serverdriven.ui.sd.data.ServerResponse
-import com.davidcorrado.serverdriven.ui.sd.data.ServerText
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.davidcorrado.serverdriven.ui.sd.data.*
 import com.davidcorrado.serverdriven.ui.sd.view.SDContent
+
+@Preview("Testing2")
+@Composable
+fun Testing2Preview() {
+    Column {
+       Surface(modifier = Modifier.size(200.dp,200.dp).background(Color.Blue)) {
+           Text("test", modifier = Modifier.background(Color.Red))
+       }
+    }
+}
 
 @Preview("Testing")
 @Composable
@@ -29,7 +48,7 @@ fun TestingPreview() {
                     )
                 ),
                 ServerCard(
-                    items = listOf(ServerText(text = "Card Padding HW")),
+                    items = listOf(ServerColumn(items = listOf(ServerText(text = "Card Padding HW"),ServerText(text = "Card Padding HW")))),
                     modifier = ServerModifier(
                         width = 200f,
                         height = 200f,
