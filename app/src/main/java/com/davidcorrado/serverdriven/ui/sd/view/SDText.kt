86e647bfdb35403ce3c33d23bb7bd34c5cfa3d5b
david.corrado <david.corrado@well.co>
2022-07-21 16:08:34 -0500

    ADA

--- app/src/main/java/com/davidcorrado/serverdriven/ui/sd/view/SDText.kt
+++ app/src/main/java/com/davidcorrado/serverdriven/ui/sd/view/SDText.kt
@@ -1,10 +1,9 @@
 package com.davidcorrado.serverdriven.ui.sd.view
 
-import androidx.compose.foundation.background
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import com.davidcorrado.serverdriven.ui.sd.data.ServerText
@@ -15,6 +14,9 @@ fun SDText(serverText: ServerText, scope: Any?) {
         style = serverText.getTextStyle(),
         text = serverText.text,
         modifier = (serverText.modifier?.toModifier(scope)
-            ?: Modifier).semantics { if (serverText.isHeading()) heading() }
+            ?: Modifier).semantics {
+            contentDescription = serverText.adaText ?: serverText.text
+            if (serverText.isHeading()) heading()
+        }
     )
 }
