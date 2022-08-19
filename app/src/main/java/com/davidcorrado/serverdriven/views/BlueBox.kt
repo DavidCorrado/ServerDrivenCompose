e318b0d6dfb3d9eee7f138fe2b64a394b6eb6f95
mustafa.alkan <mustafa.alkan@well.co>
2022-08-19 16:01:09 +0300

    updating blue box examples

--- app/src/main/java/com/davidcorrado/serverdriven/views/BlueBox.kt
+++ app/src/main/java/com/davidcorrado/serverdriven/views/BlueBox.kt
@@ -25,30 +25,79 @@ fun BlueCard(): ServerRow {
         modifier = ServerModifier(spacingStart = 20f, spacingEnd = 20f),
         subviews = listOf(
             ServerBox(
-                modifier = ServerModifier(backgroundColor = "#002F8C", cornerRadius = 16f, weight = 1f),
+                modifier = ServerModifier(
+                    backgroundColor = "#002F8C",
+                    cornerRadius = 16f,
+                    weight = 1f
+                ),
                 subviews = listOf(
                     ServerColumn(
-                        modifier = ServerModifier(
-                            spacingStart = 24f,
-                            spacingEnd = 36f,
-                            spacingTop = 61f,
-                            spacingBottom = 33f
-                        ),
                         subviews = listOf(
-                            ServerText(text = "Introducing", size = 14, weight = "600", color = "#FFFFFF"),
-                            ServerText(text = "Check-ins", size = 28, weight = "700", color = "#87C5FF"),
-                            ServerSpacer(modifier = ServerModifier(height = 22f)),
-                            ServerText(text = "Create your first entry and let’s start connecting the dots for your health.", size = 16, weight = "500", color = "#FFFFFF"),
-                            ServerSpacer(modifier = ServerModifier(height = 27f)),
-                            ServerText(text = "YOU’VE GOT A HEAD START!", size = 12, weight = "700", color = "#FFFFFF"),
-                            ServerSpacer(modifier = ServerModifier(height = 8f)),
                             ServerRow(
+                                alignment = ServerAlignment.END,
+                                subviews = listOf(
+                                    ServerSpacer(),
+                                    ServerImage(
+                                        url = R.drawable.ic_masked_check,
+                                        modifier = ServerModifier(width = 192f, height = 80f)
+                                    )
+                                )
+                            ),
+                            ServerText(
+                                modifier = ServerModifier(
+                                    spacingStart = 24f,
+                                    spacingEnd = 36f,
+                                ),
+                                text = "Check-ins",
+                                size = 28,
+                                weight = "700",
+                                color = "#87C5FF"
+                            ),
+                            ServerText(
+                                text = "Create your first entry and let’s start connecting the dots for your health.",
+                                size = 16,
+                                weight = "500",
+                                color = "#FFFFFF",
+                                modifier = ServerModifier(
+                                    spacingTop = 22f,
+                                    spacingBottom = 27f,
+                                    spacingStart = 24f,
+                                    spacingEnd = 36f,
+                                )
+                            ),
+                            ServerText(
+                                text = "YOU’VE GOT A HEAD START!",
+                                size = 12,
+                                weight = "700",
+                                color = "#FFFFFF",
+                                modifier = ServerModifier(
+                                    spacingBottom = 8f,
+                                    spacingStart = 24f,
+                                    spacingEnd = 36f,
+                                )
+                            ),
+                            ServerRow(
+                                modifier = ServerModifier(
+                                    spacingBottom = 33f,
+                                    spacingStart = 24f,
+                                    spacingEnd = 36f,
+                                ),
                                 subviews = listOf(
                                     ServerImage(
                                         url = R.drawable.ic_arrow_right,
-                                        modifier = ServerModifier(width = 10f, height = 6f, spacingTop = 5f, spacingEnd = 5f)
+                                        modifier = ServerModifier(
+                                            width = 10f,
+                                            height = 6f,
+                                            spacingTop = 5f,
+                                            spacingEnd = 5f
+                                        )
                                     ),
-                                    ServerText(text = "Some of the data you record in journeys will appear here, too.", size = 12, weight = "500", color = "#FFFFFF")
+                                    ServerText(
+                                        text = "Some of the data you record in journeys will appear here, too.",
+                                        size = 12,
+                                        weight = "500",
+                                        color = "#FFFFFF"
+                                    )
                                 )
                             )
                         )
